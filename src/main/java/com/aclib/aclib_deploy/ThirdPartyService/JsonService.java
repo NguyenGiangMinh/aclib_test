38eefa5c6e17bf0390c4e17210036438abd367fb
LouisVie61 <tantrieucao@gmail.com>
2024-11-23 00:26:20 +0700

    feat: add Json service for supporting response

--- src/main/java/com/aclib/aclib_deploy/ThirdPartyService/JsonService.java
+++ src/main/java/com/aclib/aclib_deploy/ThirdPartyService/JsonService.java
@@ -1,4 +1,27 @@
 package com.aclib.aclib_deploy.ThirdPartyService;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.stereotype.Service;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
 public class JsonService {
+
+    public void saveRegistrationIdToJson(String registrationId, String filePath) {
+        Map<String, String> data = new HashMap<>();
+        data.put("registrationId", registrationId);
+
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            objectMapper.writeValue(new File(filePath), data);
+            System.out.println("JSON file created at: " + filePath);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
+
