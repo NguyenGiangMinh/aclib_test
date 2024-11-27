aa37d94d93511420efe3709500138c2f998908b2
LouisVie61 <tantrieucao@gmail.com>
2024-11-27 16:28:58 +0700

    feat: add new port split emailService with regularService

--- src/main/java/com/aclib/aclib_deploy/ThirdPartyService/EmailAsyncService.java
+++ src/main/java/com/aclib/aclib_deploy/ThirdPartyService/EmailAsyncService.java
@@ -3,7 +3,7 @@ import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
-
+// commit again
 @Service
 public class EmailAsyncService {
 
@@ -14,7 +14,38 @@ public class EmailAsyncService {
     }
 
     @Async
-    public void sendEmailAsync(String email, String username, String otp, String registrationId, LocalDateTime expiredTime) {
-        emailService.sendOTPNotifications(email, username, otp, registrationId, expiredTime);
+    public void sendEmailAsyncOverdue(String toEmail, String userName, String bookTitle) {
+        emailService.sendOverdueNotification(toEmail, userName, bookTitle);
+    }
+
+    @Async
+    public void sendEmailAsyncAutoReturn(String toEmail, String userName, String bookTitle) {
+        emailService.sendAutoReturn(toEmail, userName, bookTitle);
+    }
+
+    @Async
+    public void setEmailAsyncNotifyAdminOfLostBook(String toEmail, String userName, String bookTitle) {
+        emailService.notifyAdminOfLostBook(toEmail, userName, bookTitle);
+    }
+
+    @Async
+    public void sendEmailAsyncMakingLoanSuccessfully(String toEmail, String userName, String bookTitle, LocalDateTime dueDate) {
+        emailService.makingLoanSuccessfully(toEmail, userName, bookTitle, dueDate);
+    }
+
+    @Async
+    public void sendEmailAsyncSendOTPSuccessfully(String toEmail, String userName,
+                                                  String OTP, String registrationId, LocalDateTime expiredTime) {
+        emailService.sendOTPNotifications(toEmail, userName, OTP, registrationId, expiredTime);
+    }
+
+    @Async
+    public void sendEmailAsyncSendRoleUpdateNotifications(String toEmail, String userName, String role) {
+        emailService.sendRoleUpdateNotifications(toEmail, userName, role);
+    }
+
+    @Async
+    public void sendEmailAsyncReturnSuccessfully(String toEmail, String userName, String bookTitle) {
+        emailService.returnSuccessfully(toEmail, userName, bookTitle);
     }
 }
